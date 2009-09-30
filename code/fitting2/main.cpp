f435d58fb32e5d1342b644f5a0aa502dbc1bd4ae
Frank S. Thomas <fthomas@physik.uni-wuerzburg.de>
2009-09-30 15:39:09 +0200

    Removed unneeded Auxiliary.f90 and ChiSquare.f90.

--- code/fitting2/main.cpp
+++ code/fitting2/main.cpp
@@ -1,12 +1,15 @@
 extern "C" {
   void mimicspheno_mp_runtill_model_bilinear_rparity_(void);
   void mimicspheno_mp_runtillend_(void);
+
+  void rptools_mp_chisquare_(void);
 }
 
 int main(int argc, char *argv[])
 {
   mimicspheno_mp_runtill_model_bilinear_rparity_();
-  mimicspheno_mp_runtillend_();
+  rptools_mp_chisquare_();
+  //mimicspheno_mp_runtillend_();
   return 0;
 }
 
