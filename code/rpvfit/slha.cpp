5c2e22aea3b3990663c90d42db5cb0cfa793bbf4
Frank S. Thomas <fthomas@physik.uni-wuerzburg.de>
2009-10-07 13:47:00 +0200

    Wrote code read a SLHA from a stream.

--- code/rpvfit/slha.cpp
+++ code/rpvfit/slha.cpp
@@ -6,20 +6,76 @@ using namespace std;
 
 void Slha::read(istream& is)
 {
+  string line;
+
+  while (getline(is, line)) {
+    // Iterate over line up to the first non-whitespace character.
+    string::const_iterator p = line.begin();
+    for (; p < line.end() && isspace(*p); p++) {}
+
+    // Ignore empty lines.
+    if (p == line.end()) {
+      continue;
+    }
+
+    // Ignore lines whose first non-whitespace character matches (#|[^BbDd\d]).
+    char c = tolower(*p);
+    if (c == '#' || (c != 'b' && c != 'd' && !isdigit(c))) {
+      continue;
+    }
+
+    cout << line << endl;
+  }
+/*
+    for (p = line.begin(); p < line.end(); p++) {
+      // Ignore leading white space.
+      if (isspace(*p)) {
+        continue;
+      }
+
+      // Don't parse comment-only lines.
+      if (*p == '#') {
+        parse_line = false;
+        break;
+      }
+
+      char c = tolower(*p);
+      if (c != 'b' && c != 'd' && !isdigit(c)) {
+        cout << c;
+        parse_line = false;
+      }
+        break;
+    }
+    //cout << line;
+    //cout << parse_line;
+
+  }
+*/
+
+
+/*
   char c;
   string line;
+  bool ignore_line = false;
 
   while(is.get(c)) {
     line += c;
 
-    if (isspace(c) && c != '\n')
-      continue;
-    
+    // ignoriere whitespace
+    //   beginnt zeile mit # oder irgendwas das nicht zahl und auch nicht b
+    //   und d ist, ignoriere zeile ... ansonsten lese zeile
+
+    if (!ignore_line && !isspace(c)) {
+      if (c == '#')
+        ignore_line = true;
+    }
+
     if (c == '\n') {
       cout << line;
       line.clear();
     }
   }
+*/
 /*
   string line;
 
