19a8a94e8e856b38d4f9c6c190cb3be58938af71
Frank S. Thomas <fthomas@physik.uni-wuerzburg.de>
2009-10-08 22:11:32 +0200

    Changed formatting.

--- code/rpvfit/slha.cpp
+++ code/rpvfit/slha.cpp
@@ -9,18 +9,16 @@ void Slha::read(istream& is)
 {
   string line;
 
-  while (getline(is, line)) {
+  while (getline(is, line))
+  {
     trim(line);
 
-    if (line.empty()) {
-      continue;
-    }
+    if (line.empty()) continue;
 
     unsigned char c = toupper((unsigned char) line[0]);
-    if (c == '#') {
-      continue;
-    }
-    if (c != 'B' && c != 'D' && !isdigit(c)) {
+    if ('#' == c) continue;
+    if ('B' != c && 'D' != c && !isdigit(c))
+    {
       cerr << "Warning (SLHA): invalid line in input: " << line << endl;
       continue;
     }
@@ -38,8 +36,10 @@ void Slha::parseLine(string& line)
   size_t comment_begin = line_uc.find("#");
   line_uc = trim_right_copy(line_uc.substr(0, comment_begin));
 
-  if (isdigit((unsigned char) line[0])) {
-    if (mCurrentBlockName.empty()) {
+  if (isdigit((unsigned char) line.at(0)))
+  {
+    if (mCurrentBlockName.empty())
+    {
       cerr << "Warning (SLHA): data line outside a (valid) block: "
            << line << endl;
       return;
@@ -47,11 +47,13 @@ void Slha::parseLine(string& line)
 
     // ?
   }
-  else if (line_uc.substr(0,6) == "BLOCK ") {
+  else if (line_uc.substr(0,6) == "BLOCK ")
+  {
     mCurrentBlockName.clear();
     string block_name = trim_left_copy(line.substr(6, line_uc.length()-6));
 
-    if (block_name.find_first_of(" \t\n\v\f\r") != string::npos) {
+    if (block_name.find_first_of(" \t\n\v\f\r") != string::npos)
+    {
       cerr << "Warning (SLHA): block name contains whitespace: "
            << block_name << endl;
       return;
