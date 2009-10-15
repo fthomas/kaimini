4be077376daed5dc18514c50abbbe2911171667e
Frank S. Thomas <fthomas@physik.uni-wuerzburg.de>
2009-10-15 15:15:08 +0200

    Merged RVFit.in inot LesHouches.in.

--- code/rpvfit/main.cpp
+++ code/rpvfit/main.cpp
@@ -1,20 +1,22 @@
+#include <boost/lexical_cast.hpp>
 #include "slha.h"
 #include "spheno.h"
 
 using namespace std;
+using namespace boost;
 
 int main(int argc, char *argv[])
 {
   Slha s;
-  s.readFile("test/LesHouches.in");
-  cout << s("sminputs");
+  s.readFile("LesHouches.in");
+  cout << lexical_cast<double>( s("RPVFitObserv")(7)[2] ) ;
 
-  //sphenodouble_mp_runtill_model_bilinear_rparity_();
+  sphenodouble_mp_runtill_model_bilinear_rparity_();
 
-  //rptools_mp_chisquare_(&inputoutput_mp_add_rparity_, &control_mp_delta_mass_,
-  //                      &sphenodouble_mp_m_gut_, &sphenodouble_mp_kont_);
+  rptools_mp_chisquare_(&inputoutput_mp_add_rparity_, &control_mp_delta_mass_,
+                        &sphenodouble_mp_m_gut_, &sphenodouble_mp_kont_);
 
-  //sphenodouble_mp_runtill_end_();
+  sphenodouble_mp_runtill_end_();
 
   return 0;
 }
