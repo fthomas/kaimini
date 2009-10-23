55b4fc5ab9192c1ea00d019ab89f0ea69b63706f
Frank S. Thomas <fthomas@physik.uni-wuerzburg.de>
2009-10-23 05:16:13 +0200

    Specify parameter uncertainties in the SLHA input file.

--- src/fcn.cpp
+++ src/fcn.cpp
@@ -1,3 +1,19 @@
+// RPVFit
+// Copyright © 2009 Frank S. Thomas <fthomas@physik.uni-wuerzburg.de>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #include <vector>
 #include <Minuit2/MnUserParameters.h>
 #include "fcn.h"
@@ -33,12 +49,18 @@ double Fcn::Up() const { return 1.; }
 
 void Fcn::setUserParameters(const Slha& input)
 {
-  upar.Add("epsilon_1", to_double(input("RVKAPPAIN")(1)[2]), 1.e-16);
-  upar.Add("epsilon_2", to_double(input("RVKAPPAIN")(2)[2]), 1.e-16);
-  upar.Add("epsilon_3", to_double(input("RVKAPPAIN")(3)[2]), 1.e-16);
-  upar.Add("v_L1", to_double(input("RVSNVEVIN")(1)[2]), 1.e-16);
-  upar.Add("v_L2", to_double(input("RVSNVEVIN")(2)[2]), 1.e-16);
-  upar.Add("v_L3", to_double(input("RVSNVEVIN")(3)[2]), 1.e-16);
+  upar.Add("epsilon_1", to_double(input("RVKAPPAIN")(1)[2]),
+    to_double(input("RPVFitUpar")(1)[3]));
+  upar.Add("epsilon_2", to_double(input("RVKAPPAIN")(2)[2]),
+    to_double(input("RPVFitUpar")(2)[3]));
+  upar.Add("epsilon_3", to_double(input("RVKAPPAIN")(3)[2]),
+    to_double(input("RPVFitUpar")(3)[3]));
+  upar.Add("v_L1", to_double(input("RVSNVEVIN")(1)[2]),
+    to_double(input("RPVFitUpar")(4)[3]));
+  upar.Add("v_L2", to_double(input("RVSNVEVIN")(2)[2]),
+    to_double(input("RPVFitUpar")(5)[3]));
+  upar.Add("v_L3", to_double(input("RVSNVEVIN")(3)[2]),
+    to_double(input("RPVFitUpar")(6)[3]));
 
   if ("0" == input("RPVFitUpar")(1)[2]) upar.Fix("epsilon_1");
   if ("0" == input("RPVFitUpar")(2)[2]) upar.Fix("epsilon_2");
