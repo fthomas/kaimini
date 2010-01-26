d698caa4498ae71e186d074772c3ec93b4473229
Frank S. Thomas <fthomas@physik.uni-wuerzburg.de>
2010-01-26 15:40:51 +0100

    Minimized main.cpp. :-)

--- src/main.cpp
+++ src/main.cpp
@@ -15,40 +15,24 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <string>
-#include <Minuit2/FunctionMinimum.h>
-#include <Minuit2/MnMinimize.h>
 #include <Minuit2/MnPrint.h>
 #include "kaimini.h"
 #include "sphenofit.h"
+#include "minuitdriver.h"
 
 using namespace std;
 using namespace Kaimini;
-using namespace ROOT::Minuit2;
 
 int main(int argc, char* argv[])
 {
-  // Use SPheno's defaul input/output filenames as our own default filenames.
   string input_file  = "LesHouches.in";
   string output_file = "SPheno.spc";
-
-  // Set our (user-supplied) i/o filenames as SPheno's i/o filenames.
   parse_command_line(argc, argv, input_file, output_file);
 
   SPhenoFit fit(input_file);
-
-  MnMinimize minimizer(fit, fit.getIntParameters());
-  FunctionMinimum minimum = minimizer();
-
-  // Call chiSquare() again with the parameter values of the minimal
-  // chi^2 to assure that the same values are set in SPheno.
-  fit.setIntParameters( minimum.UserParameters());
-  //fit.chiSquare();
-
-  cout << minimum << endl;
-
-
-  fit.writeResult(output_file);
-
+  MinuitDriver driver(&fit);
+  cout << driver.runMinimize();
+  fit.tearDown(output_file);
   return 0;
 }
 
