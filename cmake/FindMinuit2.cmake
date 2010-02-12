dbeaa453551ffbbc8887046c28f9cd88c6e231b9
Frank S. Thomas <fthomas@physik.uni-wuerzburg.de>
2010-02-12 15:34:28 +0100

    The root/include/lib dir of Minuit2 can be specified on the cmake invocation.

--- cmake/FindMinuit2.cmake
+++ cmake/FindMinuit2.cmake
@@ -1,8 +1,24 @@
-find_path(Minuit2_INCLUDE_DIRS Minuit2/FCNBase.h PATH_SUFFIXES root)
+# If Minuit2_ROOT was defined in the environment, use it.
+if(NOT Minuit2_ROOT AND NOT $ENV{Minuit2_ROOT} STREQUAL "")
+    set(Minuit2_ROOT $ENV{Minuit2_ROOT})
+endif()
 
-find_library(Minuit2_LIBRARIES Minuit2 PATH_SUFFIXES
-    root/5.08 root/5.10 root/5.12 root/5.14 root/5.16
-    root/5.18 root/5.20 root/5.21 root/5.22 root/5.24)
+if(Minuit2_ROOT)
+    set(_Minuit2_INCLUDE_SEARCH_DIRS
+        ${Minuit2_INCLUDEDIR}
+        ${Minuit2_ROOT}/include
+        ${Minuit2_ROOT})
+    set(_Minuit2_LIBRARIES_SEARCH_DIRS
+        ${Minuit2_LIBRARYDIR}
+        ${Minuit2_ROOT}/lib
+        ${Minuit2_ROOT})
+endif()
+
+find_path(Minuit2_INCLUDE_DIRS Minuit2/FCNBase.h
+    HINTS ${_Minuit2_INCLUDE_SEARCH_DIRS})
+
+find_library(Minuit2_LIBRARIES Minuit2
+    HINTS ${_Minuit2_LIBRARIES_SEARCH_DIRS})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Minuit2 DEFAULT_MSG
