dd43dc13aa650ef238ab4050706681e9182c8fbe
Frank S. Thomas <fthomas@physik.uni-wuerzburg.de>
2010-02-16 20:57:40 +0100

    Don't link to gomp if it's not required.

--- cmake/FindMinuit2.cmake
+++ cmake/FindMinuit2.cmake
@@ -19,14 +19,26 @@ endif()
 find_path(Minuit2_INCLUDE_DIRS Minuit2/FCNBase.h
     HINTS ${_Minuit2_INCLUDE_SEARCH_DIRS})
 
-find_library(Minuit2_LIBRARIES Minuit2
+set(Minuit2_LIBRARIES Minuit2)
+find_library(Minuit2_LIBRARY ${Minuit2_LIBRARIES}
     HINTS ${_Minuit2_LIBRARIES_SEARCH_DIRS})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Minuit2 DEFAULT_MSG
-    Minuit2_INCLUDE_DIRS Minuit2_LIBRARIES)
+    Minuit2_INCLUDE_DIRS Minuit2_LIBRARY)
 
-get_filename_component(Minuit2_LIBRARY_DIRS ${Minuit2_LIBRARIES} PATH)
+# If Minuit2 is built with GCC and OpenMP enabled, it contains
+# undefined references to some omp_* and GOMP_* symbols. 3rd-party
+# programs therefore need to link to gomp on their own.
+if(UNIX)
+    execute_process(COMMAND ldd -r ${Minuit2_LIBRARY}
+        ERROR_VARIABLE Minuit2_LDD_ERROR OUTPUT_QUIET)
+    if (Minuit2_LDD_ERROR MATCHES "undefined.*GOMP")
+        set(Minuit2_LIBRARIES ${Minuit2_LIBRARIES} gomp)
+    endif()
+endif()
+
+get_filename_component(Minuit2_LIBRARY_DIRS ${Minuit2_LIBRARY} PATH)
 
 mark_as_advanced(
     Minuit2_INCLUDE_DIRS
